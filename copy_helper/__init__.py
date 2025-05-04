e2990ee2bec1801d41a291d7109a3df11bf4cedc
perite <nikita.krementar@gmail.com>
2025-05-04 22:02:02 +0200

    improve file checking

--- copy_helper/__init__.py
+++ copy_helper/__init__.py
@@ -2,18 +2,26 @@ import json
 import logging
 import os
 
-from . import domain
-from . import offer
-from . import styles_helper
 
-if not os.path.exists('copy_helper/offers_info_cache.json'):
-    open('copy_helper/offers_info_cache.json', 'w').write('{}')
+def check_files():
+    if not os.path.exists('copy_helper/offers_info_cache.json'):
+        open('copy_helper/offers_info_cache.json', 'w').write('{}')
 
-if not os.path.exists('copy_helper/secrets.json'):
-    open('copy_helper/secrets.json', 'w').write(json.dumps({
-        "MONDAY_TOKEN": "",
-        "OAUTH_CLIENT": ""
-    }))
+    if not os.path.exists('copy_helper/secrets.json'):
+        open('copy_helper/secrets.json', 'w').write(json.dumps({
+            "MONDAY_TOKEN": "",
+            "OAUTH_CLIENT": ""
+        }))
 
-    logging.warning('Fill secrets file!')
-    exit()
+        logging.warning('Fill secrets file!')
+        exit()
+
+
+def import_modules():
+    from . import domain
+    from . import offer
+    from . import styles_helper
+
+
+check_files()
+import_modules()
